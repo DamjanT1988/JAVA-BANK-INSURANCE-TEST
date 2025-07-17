5affb7eea8a3f56ea3d1b4b5daade909e8fa9f92
DamjanT1988 <Damjan_t@hotmail.com>
2025-07-17 18:03:03 +0200

    Initialiserat

---
+++ gofido/src/main/java/com/example/gofido/gofido/GofidoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gofido.gofido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GofidoApplication.class, args);
+	}
+
+}
---
+++ gofido/src/test/java/com/example/gofido/gofido/GofidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gofido.gofido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
